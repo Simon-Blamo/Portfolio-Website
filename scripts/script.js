6cca3d593b9f975da85819061facc0fd42afa65b
Simon <simonblamo@gmail.com>
2023-06-25 00:05:36 -0400

    update

--- scripts/script.js
+++ scripts/script.js
@@ -68,15 +68,7 @@ const nav = document.getElementById('navbar');
 
             arrowBtns.forEach(btn => {
                 btn.addEventListener("click", () => {
-                    if(windowWidth > 1000){
-                        carousel.scrollLeft += btn.id == "left" ? (-firstCardWidth * 1.85) : (firstCardWidth * 1.85);
-                    } else if (windowWidth > 525 && windowWidth <= 1000) {
-                        carousel.scrollLeft += btn.id == "left" ? (-firstCardWidth * 2.475) : (firstCardWidth * 2.475);
-                    } else if (windowWidth <= 525) {
-                        carousel.scrollLeft += btn.id == "left" ? (-windowWidth) : (windowWidth);
-                        console.log(`The viewport's width is ${windowWidth}`);
-                    }
-                    
+                    carousel.scrollLeft += btn.id == "left" ? (-windowWidth) : (windowWidth);
                 })
             });
 
